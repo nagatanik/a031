e9b0f6a783d5977f5b99e7e86c717b8730ac582b
nagatanik <39064543+nagatanik@users.noreply.github.com>
2020-06-13 22:45:59 +0900

    fix bugs

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-bases = [2, 3, 5]
+import copy
+inputs = '2 3 5 1000'
+tmp = [int(e) for e in inputs.split()]
 
-n = 7
+bases = tmp[:-1]
+k = tmp[-1]
 
-def check_factor(value, bases):
-    is_factor = False
-    for b in bases:
-        if value % b == 0:
-            is_factor = True
-            break
-    return is_factor
-            
 
-value = 1
-k = 1
-while k < n:
-    value += 1
-    if check_factor(value, bases):
-        k += 1
+dic_values = {}
+dic_used = {}
+dic_cand = {}
+
+initial_key = (0, 0, 0)
+initial_value = 1
+
+dic_cand[initial_key] = initial_value
+dic_values[initial_key] = initial_value
+
+idx = 1
+while True:
+    key, value = min(dic_cand.items(), key=lambda x: x[1])
+    del dic_cand[key]
+    if idx == k:
+        break
+
+    idx += 1
+
+    for i, base in enumerate(bases):
+        key_n = list(copy.copy(key))
+        key_n[i] += 1
+        if tuple(key_n) in dic_cand: continue
+
+        dic_cand[tuple(key_n)] = value * base
 
 print(value)
+    
