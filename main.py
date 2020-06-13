621308845d408b9bfb519c546983d3ba0545650f
nagatanik <39064543+nagatanik@users.noreply.github.com>
2020-06-13 14:01:05 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+bases = [2, 3, 5]
+
+
+n = 7
+list_values = [1]
+
+def check_factor(value, bases):
+    is_factor = False
+    for b in bases:
+        if value // b == 0:
+            is_factor = True
+            break
+    return is_factor
+            
+
+value = 1
+k = 1
+while k < n:
+    value += 1
+    if check_factor:
+        k += 1
+
+print(value)
